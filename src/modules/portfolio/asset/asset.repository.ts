b54f846221bd257bd95d2ac7382c4e80ca214933
SONGSEUNGSOO <sssong1993@gmail.com>
2025-09-02 20:55:52 +0900

    refactor: Repository 로깅 제거 및 자산 삭제 로직 구현
    - asset-history repository에서 모든 console.log 제거
    - asset repository에 deleteAsset 메서드 구현
    - 자산 삭제를 위한 트랜잭션 처리 추가
    - UserAsset 삭제 시 AssetHistory 자동 삭제 지원

--- src/modules/portfolio/asset/asset.repository.ts
+++ src/modules/portfolio/asset/asset.repository.ts
@@ -1,6 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { AssetHistory } from 'src/database/entities/portfolio/asset-history.entity';
+import { UserAsset } from 'src/database/entities/portfolio/user-asset.entity';
+import { DataSource } from 'typeorm';
 
 @Injectable()
 export class AssetRepository {
-  constructor() {}
+  constructor(private readonly dataSource: DataSource) {}
+
+  async deleteAsset(userAssetId: number) {
+    const queryRunner = this.dataSource.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+
+    try {
+      // 1. AssetHistory 삭제
+      await queryRunner.manager.delete(AssetHistory, {
+        user_asset_id: userAssetId,
+      });
+
+      // 2. UserAsset 삭제
+      await queryRunner.manager.delete(UserAsset, {
+        id: userAssetId,
+      });
+
+      await queryRunner.commitTransaction();
+      await queryRunner.release();
+    } catch (error) {
+      await queryRunner.rollbackTransaction();
+      throw error;
+    }
+  }
 }
